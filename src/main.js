b7edbf7e85b4fefc74de2640ae43b7f828093f45
Aaron <aaronlee120213@gmail.com>
2023-02-20 18:53:34 +0800

    add nav

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+// import "/scss/customize.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 createApp(App).use(router).mount("#app");
+
+import "bootstrap/dist/js/bootstrap.js";
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about container">
+    <div class="text-pretso-blue">This is an pretso-blue</div>
+    <div class="text-pretso-orange">This is an pretso-orange</div>
+    <button type="button" class="btn btn-pretso-orange">pretso-orange</button>
+    <button type="button" class="btn btn-pretso-blue">pretso-blue</button>
+    <button type="button" class="btn btn-error-red">error-red</button>
   </div>
 </template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
+<style lang="scss" scoped>
+// @import "/scss/customize.scss";
+.about {
+  width: 1700px;
+}
+</style>
